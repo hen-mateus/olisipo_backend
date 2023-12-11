d38e28fee229132fa9b63f89a03866908e6f9173
Henrique Mateus <henriquemateus.prof@gmail.com>
2023-12-11 17:09:17 +0000

    Update Controller Cron - Recibo

--- server/src/controllers/recibos_vencimentoController.js
+++ server/src/controllers/recibos_vencimentoController.js
@@ -1,11 +1,12 @@
 const Sequelize = require('sequelize');
 const sequelize = require('../models/database');
+const cron = require('node-cron');
 
 const recibosVencimentoController = {};
 
 recibosVencimentoController.list = async (req, res) => {
     try {
-        const query = 'SELECT recibos_vencimento.*, pessoas.nome_pessoa FROM recibos_vencimento INNER JOIN pessoas ON recibos_vencimento.id_pessoa = pessoas.id_pessoa;';
+        const query = 'SELECT recibos_vencimento.*, pessoas.nome_pessoa FROM recibos_vencimento INNER JOIN pessoas ON recibos_vencimento.id_pessoa = pessoas.id_pessoa AND confirmacao_submissao_recibo = false;';
         const data = await sequelize.query(query, { type: Sequelize.QueryTypes.SELECT });
 
         res.json({ success: true, data: data });
@@ -89,5 +90,41 @@ recibosVencimentoController.update = async (req, res) => {
         res.status(500).json({ success: false, error: error.message });
     }
 };
+
+async function criarRecibosParaTodasPessoas() {
+    try {
+        // Obtém todas as pessoas da tabela pessoas
+        const pessoas = await sequelize.query('SELECT id_pessoa FROM pessoas', {
+            type: Sequelize.QueryTypes.SELECT
+        });
+
+        // Itera sobre cada pessoa e chama o procedimento para inserir o recibo
+        for (const pessoa of pessoas) {
+            const { id_pessoa } = pessoa;
+
+            // Chama o procedimento InserirReciboVencimento para cada pessoa
+            await sequelize.query(`
+                CALL InserirReciboVencimento(
+                    NULL,
+                    NULL,
+                    ${id_pessoa},
+                    false,
+                    CURRENT_DATE
+                )
+            `);
+        }
+
+        console.log('Recibos criados para todas as pessoas com sucesso!');
+    } catch (error) {
+        console.error('Erro ao criar recibos:', error);
+    }
+}
+
+cron.schedule('45 16 11 * *', () => {
+    criarRecibosParaTodasPessoas();
+}, {
+    scheduled: true,
+    timezone: 'Europe/Lisbon'
+});
 
 module.exports = recibosVencimentoController;
