78b37f401afe64801ec30774b4432257ea5088e0
Henrique Mateus <henriquemateus.prof@gmail.com>
2024-01-16 21:24:52 +0000

    Update appMobileRoute.js

--- server/src/routes/appMobileRoute.js
+++ server/src/routes/appMobileRoute.js
@@ -4,6 +4,6 @@ const appMobileController = require('../controllers/appMobileController');
 const { createTokens, validateToken } = require("../jwt");
 
 
-router.get('/', appMobileController.list);
+router.get('/', validateToken, appMobileController.list);
 
 module.exports = router;
