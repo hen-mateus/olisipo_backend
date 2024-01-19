350d775aad9eda1e7f85ebf26a9a252ea0ed2e72
Nuno Santos <nunoabelss24@gmail.com>
2024-01-19 23:19:36 +0000

    Update reuniaoRHRoute.js

--- server/src/routes/reuniaoRHRoute.js
+++ server/src/routes/reuniaoRHRoute.js
@@ -5,6 +5,7 @@ const reuniaoRHController = require('../controllers/reuniao_rhController');
 const { createTokens, validateToken } = require("../jwt");
 
 router.get('/', reuniaoRHController.list);
+router.get('/listmanager', validateToken, reuniaoRHController.listManagers);
 router.get('/:id', reuniaoRHController.getId);
 router.post('/create/', validateToken, reuniaoRHController.create);
 router.put('/update/:id', validateToken, reuniaoRHController.update);
