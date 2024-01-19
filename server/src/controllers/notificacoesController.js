17f352c4f2aa2339062114f5465239ee6b51150a
Nuno Santos <nunoabelss24@gmail.com>
2024-01-19 23:06:45 +0000

    Update notificacoesController.js

--- server/src/controllers/notificacoesController.js
+++ server/src/controllers/notificacoesController.js
@@ -14,29 +14,6 @@ notificacoesController.list = async (req, res) => {
     }
 };
 
-notificacoesController.listManager = async (req, res) => {
-    const id_pessoa_param = req.userId;
-
-    try {
-        const query = `SELECT tipos_possiveis.tipo_notificacao, 
-       COALESCE(COUNT(notificacoes.id_notificacao), 0) AS quantidade_nao_lida 
-FROM (
-    SELECT 'Reuniões' AS tipo_notificacao 
-) AS tipos_possiveis 
-LEFT JOIN notificacoes ON tipos_possiveis.tipo_notificacao = notificacoes.tipo_notificacao 
-                      AND notificacoes.notificacao_lida = FALSE 
-WHERE notificacoes.id_pessoa = ${id_pessoa_param}
-GROUP BY tipos_possiveis.tipo_notificacao;
-
-          `;
-        const data = await sequelize.query(query, { type: Sequelize.QueryTypes.SELECT });
-
-        res.json({ success: true, data: data });
-    } catch (error) {
-        res.status(500).json({ success: false, error: error.message });
-    }
-};
-
 notificacoesController.marcarTodasComoLidas = async (req, res) => {
     try {
         // Atualiza todas as notificações para notificacao_lida = true
